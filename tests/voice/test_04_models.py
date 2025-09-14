eda02c1ff8768a98e06c7b6788c56be94d653e62
Sam Sykes <sams@speechmatics.com>
2025-09-14 15:23:44 +0100

    ADAPTIVE end of utterance tests

--- tests/voice/test_04_models.py
+++ tests/voice/test_04_models.py
@@ -12,8 +12,10 @@ from _utils import get_client
 from speechmatics.voice import AgentServerMessageType
 from speechmatics.voice import AnnotationFlags
 from speechmatics.voice import AnnotationResult
+from speechmatics.voice import EndOfUtteranceMode
 from speechmatics.voice import SpeakerSegment
 from speechmatics.voice import SpeechFragment
+from speechmatics.voice import VoiceAgentConfig
 
 
 @pytest.mark.asyncio
@@ -26,12 +28,10 @@ async def test_annotation_result():
     """
 
     # Create a new annotation
-    annotation = AnnotationResult()
+    annotation = AnnotationResult.from_flags(AnnotationFlags.NO_TEXT, AnnotationFlags.HAS_DISFLUENCY)
     assert annotation is not None
 
-    # Add flags
-    annotation.add(AnnotationFlags.NO_TEXT)
-    annotation.add(AnnotationFlags.HAS_DISFLUENCY)
+    # Add extra flag
     annotation.add(AnnotationFlags.MULTIPLE_SPEAKERS)
 
     # Has a flag
@@ -127,9 +127,9 @@ async def test_speech_fragments():
     client.once(AgentServerMessageType.ADD_INTERIM_SEGMENTS, message_rx)
 
     # Inject first partial
-    await send_message(2, use_ttl=False)
+    await send_message(0, count=3, use_ttl=False)
 
-    # Wait for partial transcript
+    # Wait for first segment
     try:
         await asyncio.wait_for(event_rx.wait(), timeout=5.0)
         assert last_message is not None
@@ -151,9 +151,9 @@ async def test_speech_fragments():
     client.once(AgentServerMessageType.ADD_SEGMENTS, message_rx)
 
     # Send a more partials and finals
-    await send_message(3, count=11, use_ttl=False)
+    await send_message(3, count=10, use_ttl=False)
 
-    # Wait for partial transcript
+    # Wait for final segment
     try:
         await asyncio.wait_for(event_rx.wait(), timeout=5.0)
         assert last_message is not None
@@ -173,6 +173,230 @@ async def test_speech_fragments():
     # Check the contents of the segment
     fragments: list[SpeechFragment] = segments[0].fragments
     assert len(fragments) == 4
+
+
+@pytest.mark.asyncio
+async def test_end_of_utterance_fixed():
+    """Test EndOfUtterance from STT engine.
+
+    - send converstaion messages (fast)
+    - wait for `EndOfUtterance` message
+    """
+
+    # Test conversation
+    log = ConversationLog(os.path.join(os.path.dirname(__file__), "./assets/chat2.jsonl"))
+    chat = log.get_conversation(["AddPartialTranscript", "AddTranscript", "EndOfUtterance"])
+
+    # Start time
+    start_time = datetime.datetime.now()
+
+    # Create a client
+    client = await get_client(api_key="NONE", connect=False)
+    assert client is not None
+
+    # Event to wait
+    event_rx: asyncio.Event = asyncio.Event()
+    last_message: Optional[dict[str, Any]] = None
+
+    # Message receiver
+    def message_rx(message: dict[str, Any]):
+        nonlocal last_message
+        last_message = message
+        event_rx.set()
+
+    # Send a message from the conversation
+    async def send_message(idx: int, count: int = 1, use_ttl: bool = True):
+        for i in range(count):
+            # Get the message from the chat
+            message = chat[idx + i]
+
+            # Wait for TTL to expire
+            if use_ttl:
+                ttl = (start_time + datetime.timedelta(seconds=message["ts"])) - datetime.datetime.now()
+                if ttl.total_seconds() > 0:
+                    await asyncio.sleep(ttl.total_seconds())
+            else:
+                await asyncio.sleep(0.005)
+
+            # Emit the message
+            client.emit(message["payload"]["message"], message["payload"])
+
+    # Add listener for first interim segment
+    client.once(AgentServerMessageType.END_OF_UTTERANCE, message_rx)
+
+    # Inject conversation
+    await send_message(0, count=13, use_ttl=False)
+
+    # Wait for EndOfUtterance
+    try:
+        await asyncio.wait_for(event_rx.wait(), timeout=5.0)
+        assert last_message is not None
+    except asyncio.TimeoutError:
+        pytest.fail("END_OF_UTTERANCE event was not received within 5 seconds")
+
+    # Check the right message was received
+    assert last_message.get("message") == AgentServerMessageType.END_OF_UTTERANCE
+
+
+@pytest.mark.asyncio
+async def test_external_vad():
+    """Test EndOfUtterance from STT engine.
+
+    - send converstaion messages (realtime)
+    - finalizes based on external VAD (e.g. Pipecat's `UserStoppedSpeakingFrame` frame)
+    """
+
+    # Test conversation
+    log = ConversationLog(os.path.join(os.path.dirname(__file__), "./assets/chat2.jsonl"))
+    chat = log.get_conversation(["AddPartialTranscript", "AddTranscript", "EndOfUtterance"])
+
+    # Start time
+    start_time = datetime.datetime.now()
+
+    # Adaptive timeout
+    adaptive_timeout = 1.0
+
+    # Create a client
+    client = await get_client(
+        api_key="NONE",
+        connect=False,
+        config=VoiceAgentConfig(
+            end_of_utterance_silence_trigger=adaptive_timeout, end_of_utterance_mode=EndOfUtteranceMode.NONE
+        ),
+    )
+    assert client is not None
+
+    # Event to wait
+    event_rx: asyncio.Event = asyncio.Event()
+    last_message: Optional[dict[str, Any]] = None
+
+    # Message receiver
+    def message_rx(message: dict[str, Any]):
+        nonlocal last_message
+        last_message = message
+        event_rx.set()
+
+    # Send a message from the conversation
+    async def send_message(idx: int, count: int = 1, use_ttl: bool = True):
+        for i in range(count):
+            # Get the message from the chat
+            message = chat[idx + i]
+
+            # Wait for TTL to expire
+            if use_ttl:
+                ttl = (start_time + datetime.timedelta(seconds=message["ts"])) - datetime.datetime.now()
+                if ttl.total_seconds() > 0:
+                    await asyncio.sleep(ttl.total_seconds())
+            else:
+                await asyncio.sleep(0.005)
+
+            # Emit the message
+            client.emit(message["payload"]["message"], message["payload"])
+
+    # Inject conversation
+    await send_message(0, count=11, use_ttl=False)
+
+    # Momentary pause
+    await asyncio.sleep(0.5)
+
+    # Add listener for first interim segment
+    client.once(AgentServerMessageType.ADD_SEGMENTS, message_rx)
+
+    # Send finalize
+    client.finalize()
+
+    # Wait for AddSegments
+    try:
+        await asyncio.wait_for(event_rx.wait(), timeout=4)
+        assert last_message is not None
+    except asyncio.TimeoutError:
+        pytest.fail("ADD_SEGMENTS event was not received within 4 seconds")
+
+    # Check the right message was received
+    assert last_message.get("message") == AgentServerMessageType.ADD_SEGMENTS
+
+    # Check the segment
+    segments: list[SpeakerSegment] = last_message.get("segments", [])
+    assert len(segments) == 1
+    assert segments[0].speaker_id == "S1"
+    assert segments[0].text == "Welcome to Speechmatics"
+    assert segments[0].format_text("{speaker_id}: {text}") == "S1: Welcome to Speechmatics"
+
+    # Check the contents of the segment
+    fragments: list[SpeechFragment] = segments[0].fragments
+    assert len(fragments) == 3
+
+
+@pytest.mark.asyncio
+async def test_end_of_utterance_adaptive_vad():
+    """Test EndOfUtterance from STT engine.
+
+    - send converstaion messages (realtime)
+    - wait for `EndOfUtterance` message from SDK (adaptive)
+    """
+
+    # Test conversation
+    log = ConversationLog(os.path.join(os.path.dirname(__file__), "./assets/chat2.jsonl"))
+    chat = log.get_conversation(["AddPartialTranscript", "AddTranscript", "EndOfUtterance"])
+
+    # Start time
+    start_time = datetime.datetime.now()
+
+    # Adaptive timeout
+    adaptive_timeout = 0.5
+
+    # Create a client
+    client = await get_client(
+        api_key="NONE",
+        connect=False,
+        config=VoiceAgentConfig(
+            end_of_utterance_silence_trigger=adaptive_timeout, end_of_utterance_mode=EndOfUtteranceMode.ADAPTIVE
+        ),
+    )
+    assert client is not None
+
+    # Event to wait
+    event_rx: asyncio.Event = asyncio.Event()
+    last_message: Optional[dict[str, Any]] = None
+
+    # Message receiver
+    def message_rx(message: dict[str, Any]):
+        nonlocal last_message
+        last_message = message
+        event_rx.set()
+
+    # Send a message from the conversation
+    async def send_message(idx: int, count: int = 1, use_ttl: bool = True):
+        for i in range(count):
+            # Get the message from the chat
+            message = chat[idx + i]
+
+            # Wait for TTL to expire
+            if use_ttl:
+                ttl = (start_time + datetime.timedelta(seconds=message["ts"])) - datetime.datetime.now()
+                if ttl.total_seconds() > 0:
+                    await asyncio.sleep(ttl.total_seconds())
+            else:
+                await asyncio.sleep(0.005)
+
+            # Emit the message
+            client.emit(message["payload"]["message"], message["payload"])
+
+    # Add listener for first interim segment
+    client.once(AgentServerMessageType.END_OF_UTTERANCE, message_rx)
+
+    # Inject conversation
+    await send_message(0, count=12, use_ttl=False)
+
+    # Wait for EndOfUtterance
+    try:
+        await asyncio.wait_for(event_rx.wait(), timeout=adaptive_timeout * 1.5)
+        assert last_message is not None
+    except asyncio.TimeoutError:
+        pytest.fail(f"END_OF_UTTERANCE event was not received within {adaptive_timeout * 1.5} seconds")
+
+    # Check the right message was received
+    assert last_message.get("message") == AgentServerMessageType.END_OF_UTTERANCE
 
 
 @pytest.mark.asyncio
