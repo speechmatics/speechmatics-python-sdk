f7729dfe122333fcbf612203259e7eb58fee2f49
Sam Sykes <sams@speechmatics.com>
2025-10-15 21:16:40 +0100

    update file path for RT example

--- examples/rt/async/file/main.py
+++ examples/rt/async/file/main.py
@@ -13,7 +13,7 @@ async def main():
             print(f"Final: {msg['metadata']['transcript']}")
 
         # Transcribe audio file
-        with open("./examples/example.wav", "rb") as audio_file:
+        with open("./examples/example1.wav", "rb") as audio_file:
             await client.transcribe(audio_file)
 
 
