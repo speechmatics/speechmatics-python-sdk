935c6a658a76ea40c8b1c41f62fab9a6380b1659
lgavincrl <lucy.gavin@speechmatics.com>
2025-10-29 11:52:45 +0000

    Fix async TTS Test awaits

--- tests/tts/async_http_test.py
+++ tests/tts/async_http_test.py
@@ -9,7 +9,5 @@ from speechmatics.tts import AsyncClient
 @pytest.mark.skipif(os.getenv("SPEECHMATICS_API_KEY") is None, reason="Skipping test if API key is not set")   
 async def test_async_http():
     async with AsyncClient() as client:
-        response = await client.generate(text="Hello, this is the Speechmatics TTS API. We are excited to have you here!")
-        assert response.status == 200
-        print(response)
-        assert await response.json() is not None
+        response = await client.generate(text="Hello world")
+    assert response.status == 200
