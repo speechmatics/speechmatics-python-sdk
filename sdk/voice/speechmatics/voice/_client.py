766fd385fe1c97a21877b598239901ecd33516df
Sam Sykes <sams@speechmatics.com>
2026-07-26 20:29:53 +0100

    test to remove `timestamp` from voice-sdk FEOU messages

--- sdk/voice/speechmatics/voice/_client.py
+++ sdk/voice/speechmatics/voice/_client.py
@@ -1705,8 +1705,8 @@ class VoiceAgentClient(AsyncClient):
             start_time = time.time()
             self._forced_eou_active = True
 
-            # Send the force EOU and wait for the response
-            await self.force_end_of_utterance(compensate_latency=self._feou_latency_compensation)
+            # Send the force EOU and wait for the response (and EXCLUDE the timestamp)
+            await self.force_end_of_utterance(compensate_latency=self._feou_latency_compensation, timestamp=None)
             self._emit_diagnostic_message("ForceEndOfUtterance sent - waiting for EndOfUtterance")
 
             # Wait for the response
